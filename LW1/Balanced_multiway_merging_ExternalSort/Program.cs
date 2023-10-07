f9dea628875da63da46f04cf9726d3338cb56043
Savelii Korniiko <107639922+KorSav@users.noreply.github.com>
2023-10-07 13:35:15 +0300

    Program.cs

--- LW1/Balanced_multiway_merging_ExternalSort/Program.cs
+++ LW1/Balanced_multiway_merging_ExternalSort/Program.cs
@@ -2,22 +2,6 @@
 using ExternalSortingAlgorithm;
 using nmspcHelpFuncs;
 
-/*
- * 50, 100, 3: 51 s
- * 50, 100, 5: 55 s
- * 50, 100, 7: 60 s
- * 512, 100, 3: 51 s
- * 30, 100, 3: 62 s
- * 30, 200, 3: 61 s 
- * 30, 100, 3: 30 s 
- * 512, 1024, 3: 2.53 min - 504 mb used/ sorted VVV
- * 30, 100, 3: 20.1 s 
- * 43.427s 42.505s 41.354s 30s(reverse)
- * 512, 1024, 3: 2 min (right order)
- * 512, 1024, 3: 2.35 min (reverse order)
- * 512, 1024, 3: 2.46 min (random order)
- */
-
 ExternalSort externalSort = new ExternalSort();
 HelpFuncs helpFuncs = new HelpFuncs();
 double elapsedTime;
